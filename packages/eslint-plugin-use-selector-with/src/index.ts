b761db7d597dd865c73c368c1a59c25695d95cb8
Josh Goldberg <josh@codecademy.com>
2021-09-27 12:59:56 -0400

    Fix: eslint rule names

--- packages/eslint-plugin-use-selector-with/src/index.ts
+++ packages/eslint-plugin-use-selector-with/src/index.ts
@@ -2,8 +2,8 @@ import preferUseSelectorWith from './rules/prefer-use-selector-with';
 import unnecessaryUseSelectorWith from './rules/unnecessary-use-selector-with';
 
 const rules = {
-  'use-selector-with/prefer-use-selector-with': preferUseSelectorWith,
-  'use-selector-with/unnecessary-use-selector-with': unnecessaryUseSelectorWith,
+  'prefer-use-selector-with': preferUseSelectorWith,
+  'unnecessary-use-selector-with': unnecessaryUseSelectorWith,
 };
 
 module.exports = {
